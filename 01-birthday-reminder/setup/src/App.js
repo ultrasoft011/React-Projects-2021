d7a6b0fd0e21bb2a83687a831c08966dffb637a4
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-17 23:09:37 -0500

    destructuring de props

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
   // useState Hook: allow to add the state to the function components
   // example will have the value of 'data'
   const [example, myExample] = useState(data);
-  return <main><section>
-    <h3>
-      0 Birthday today
-      <List />
-    </h3>
-    </section></main>;
+  return (
+    <main>
+      <section className="container">
+        <h3>{example.length} Birthday today</h3>
+        {/* props for List */}
+        <List example = {example}/>
+        <button onClick={() => console.log("Press me")}>
+          Clear information
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ example }) => {
+  // Destructuring the example
+  const { id, name, age, image } = example;
   return (
     <>
       <h2>list component</h2>
+      <h2>{example[0].id}</h2>
     </>
   );
 };

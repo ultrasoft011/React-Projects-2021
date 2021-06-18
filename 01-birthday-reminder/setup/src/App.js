f6ac68998339b558a8b19bae2e0601213ab6cce6
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-17 21:45:47 -0500

    useState hook

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  // useState Hook: allow to add the state to the function components
+  // example will have the value of 'data'
+  const [example, myExample] = useState(data);
+  return <main><section>
+    <h3>
+      0 Birthday today
+      <List />
+    </h3>
+    </section></main>;
 }
 
 export default App;
